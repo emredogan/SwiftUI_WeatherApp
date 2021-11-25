463700776e3c95b6290e27bb79b8d52238e8e3c3
Emre Dogan <emredoga@outlook.com>
2021-11-25 16:37:38 +0100

    Initial Commit

---
+++ WeatherApp_SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp_SwiftUI
+//
+//  Created by Emre Dogan on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
